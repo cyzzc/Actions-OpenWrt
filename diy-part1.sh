decdb95cdd72d067644de2981741808d748167dd
cyzzc <80954721+cyzzc@users.noreply.github.com>
2022-05-16 14:32:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,14 +10,9 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
 echo 'src-git nezha https://github.com/Erope/openwrt_nezha' >>feeds.conf.default
 echo 'src-git netspeedtest  https://github.com/sirpdboy/netspeedtest' >>feeds.conf.default
-echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
-
-# 添加opentopd主题
-rm -rf package/lean/luci-theme-opentopd
-git clone https://github.com/sirpdboy/luci-theme-opentopd package/lean/luci-theme-opentopd
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git cyzzc https://github.com/cyzzc/auto-cyzzc' >>feeds.conf.default
